25ac77f6aa9718cb516e6c838e9f5a3886891f31
ConsLau <hiulau106@gmail.com>
2024-11-03 15:28:51 +1100

    create reusable form components

--- actions/create-board/index.ts
+++ actions/create-board/index.ts
@@ -25,7 +25,6 @@ const handler = async (data: InputType): Promise<ReturnType> => {
     let board;
 
     try{
-        throw new Error("blablabla")
         board = await db.board.create({
             data: {
                 title,
