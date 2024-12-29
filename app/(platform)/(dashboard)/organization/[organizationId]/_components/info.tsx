0a89a5509a0fc087c7b872f42e2d7fa78868609b
ConsLau <hiulau106@gmail.com>
2024-12-30 02:01:48 +1100

    temp remove billing feature Phase 1 complete

--- app/(platform)/(dashboard)/organization/[organizationId]/_components/info.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/_components/info.tsx
@@ -29,10 +29,10 @@ export const Info = () => {
                 <p className="font-semibold text-xl">
                     {organization?.name}
                 </p>
-                <div className="flex items-center text-xs text-muted-foreground">
+                {/* <div className="flex items-center text-xs text-muted-foreground">
                     <CreditCard className="h-3 w-3 mr-1"/>
                     Free
-                </div>
+                </div> */}
             </div>
         </div>
     )
