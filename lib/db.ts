4bad5115d379cb3bd25877ac319b4ba3498b4219
ConsLau <hiulau106@gmail.com>
2025-01-08 23:15:38 +1100

    deployment - 2.2 attemp fix

--- lib/db.ts
+++ lib/db.ts
@@ -1,9 +1,28 @@
-import { PrismaClient } from "@prisma/client"
+// import { PrismaClient } from "@prisma/client"
+
+// declare global {
+//     var prisma: PrismaClient | undefined
+// }
+
+// export const db = globalThis.prisma || new PrismaClient();
+
+// if (process.env.NODE_ENV !== "production") globalThis.prisma = db;
+
+import { PrismaClient } from "@prisma/client";
 
 declare global {
-    var prisma: PrismaClient | undefined
+  var prisma: PrismaClient | undefined;
 }
 
-export const db = globalThis.prisma || new PrismaClient();
+let prisma: PrismaClient;
 
-if (process.env.NODE_ENV !== "production") globalThis.prisma = db;
+if (process.env.NODE_ENV === "production") {
+    prisma = new PrismaClient();
+} else {
+    if (!global.prisma) {
+        global.prisma = new PrismaClient();
+    }
+    prisma = global.prisma;
+}
+
+export const db = prisma;
